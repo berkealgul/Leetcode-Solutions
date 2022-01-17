b21191c2c3525b365f62bb9337ae0d01feef6637
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-01-18 01:35:58 +0300

    Update 279.perfect-squares.cpp

--- 279.perfect-squares.cpp
+++ 279.perfect-squares.cpp
@@ -2,13 +2,22 @@ class Solution {
 public:
     int numSquares(int n) 
     {
-        vector<int> nums(0,n);
+        vector<int> nums(n+1,n+1);
+        nums[1] = 0;
+        nums[0] = 0;
         
-        for(int i = 0; i < n; i++)
+        for(int i = 1; i < n+1; i++)
         {
+            int k = 1;
             
+            while(k*k <= i)
+            {
+                nums[i] = min(nums[i-k*k], nums[i]);
+                k++;
+            }
+            nums[i]++;
         }
         
-        return *nums.end();
+        return nums[nums.size()-1];
     }
 };
