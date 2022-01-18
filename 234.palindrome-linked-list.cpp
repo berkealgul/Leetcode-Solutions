f1ce1b46a6b9efb89c01b136a77270f1ffa23212
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-01-18 03:28:55 +0300

    Update 234.palindrome-linked-list.cpp

--- 234.palindrome-linked-list.cpp
+++ 234.palindrome-linked-list.cpp
@@ -1,29 +1,60 @@
-class Solution 
-{
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
-    bool isPalindrome(ListNode* head)
-    {
-        vector<ListNode*> vals;
-        int i = 0;<
-
-        while(head)
-        {
-            vals.push_back(head);
-        }
-
-        int a = 0;
-        int b = vals.size();
-
-        while(a != b)
-        {
-            if(vals[a]->val != vals[b]->val)
-                return false;
-                
-            a++;
-            b--;
+    
+    ListNode* reverseList(ListNode *head) {
+        
+        if(!head or !head->next) {
+            return head;
         }
         
+        ListNode *curr = head, *prev = NULL, *nxt = NULL;
+        
+        while(curr) {
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+    
+    
+    bool isPalindrome(ListNode* head) {
+        if(!head or !head->next) {
+            return true;
+        }
+        
+        ListNode *fast = head, *slow = head, *prevElement = NULL;
+        
+        while(fast and fast->next) {
+            prevElement = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        prevElement->next = NULL;
+        
+        ListNode *halfHead = reverseList(slow);
+        
+        ListNode *p = head, *q = halfHead;
+        
+        while(p and q) {
+            if(p->val != q->val) {
+                return false;
+            }
+            p = p->next;
+            q = q->next;
+        }
         return true;
-
     }
 };
