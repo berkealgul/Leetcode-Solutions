b0d4a97149eb052951e889375f37a99a281b2f8f
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-05-11 20:20:35 +0300

    Add files via upload

---
+++ 111.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    int minDepth(TreeNode* root) 
+    {
+        if(!root)
+            return 0; // max number
+        
+        int d_l = minDepth(root->left);
+        int d_r = minDepth(root->right);
+
+        if(d_l == 0)
+            return ++d_r;
+        else if(d_r == 0)
+            return ++d_l;
+        else
+            return min(d_l, d_r)+1;
+
+    }
+};
