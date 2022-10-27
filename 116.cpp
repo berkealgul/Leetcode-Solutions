eccdfdc31acf56b084e7425e78a2bb671821ef70
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-10-27 09:37:42 +0300

    Add files via upload

---
+++ 116.cpp
@@ -0,0 +1,49 @@
+class Solution 
+{
+public:
+    Node* connect(Node* root) 
+    {
+        if(!root) {return root;}
+
+        int count = 1;
+        int limit = 1;
+        queue<Node*> set;
+        Node* node = root;
+        
+        set.push(root);
+        
+        while(!set.empty())
+        {
+            cout << node->val << " ";
+            
+            if(count == limit)
+            {
+                count = 0;
+                limit *= 2;
+                set.pop();
+            }
+            else
+            {
+                node = set.front();
+                set.pop();
+                node->next = set.front();
+            }
+            
+            
+            if(node->left)
+            {
+                set.push(node->left);
+                set.push(node->right);
+            }
+
+            node = set.front();
+            count++;
+            
+        }
+        
+
+        return root;
+    }
+};
+
+
